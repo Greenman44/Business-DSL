4a409503e277a3ffb003ad04d98813d845566306
Abraham <gonzalezriveroabraham@gmail.com>
2023-02-09 12:45:55 -0500

    Dataframe added

--- language/types.py
+++ language/types.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class Employee:
     def __init__(self, name, salary):
         self.name = name
@@ -12,10 +13,16 @@ class Product:
 class Staff:    
     def __init__(self, employees: list[Employee]):
         self.employees = employees
+        self.df = pd.DataFrame.from_dict(employees[0].__dict__)
+        for i in range(len(employees) - 1):
+            self.df = self.df.append(employees[i+1].__dict__)
 
 class Catalog:
     def __init__(self, products: list[Product]):
         self.products = products
+        self.df = pd.DataFrame.from_dict(products[0].__dict__)
+        for i in range(len(products) - 1):
+            self.df = self.df.append(products[i+1].__dict__)
         
 class Business:
     def __init__(self, name, staff: Staff, catalog : Catalog):
